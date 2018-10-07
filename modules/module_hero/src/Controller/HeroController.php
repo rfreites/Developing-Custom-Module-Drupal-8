444dd4a306801afcc779cb8312235a409a0ce279
Ronny <ronnyangelo.freites@gmail.com>
2018-10-07 17:05:04 -0600

    Add HeroController twig template and libraries

--- modules/module_hero/src/Controller/HeroController.php
+++ modules/module_hero/src/Controller/HeroController.php
@@ -7,37 +7,33 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class HeroController extends ControllerBase{
   public function heroList() {
+
     $heroes = [
-      'name' => 'Ant-Man',
-      'name' => 'Aquaman',
-      'name' => 'Asterix',
-      'name' => 'The Atom',
-      'name' => 'The Avengers',
-      'name' => 'Batgirl',
-      'name' => 'Batman',
-      'name' => 'Batwoman',
-      'name' => 'Black Canary',
-      'name' => 'Black Panther',
-      'name' => 'Captain America',
-      'name' => 'Catwoman',
-      'name' => 'Conan the Barbarian',
-      'name' => 'Daredevil',
-      'name' => 'The Defenders',
-      'name' => 'Doc Savage',
-      'name' => 'Doctor Strange',
-      'name' => 'Elektra',
-      'name' => 'Fantastic Four'
+      ['name' => 'Ant-Man'],
+      ['name' => 'Aquaman'],
+      ['name' => 'Asterix'],
+      ['name' => 'The Atom'],
+      ['name' => 'The Avengers'],
+      ['name' => 'Batgirl'],
+      ['name' => 'Batman'],
+      ['name' => 'Batwoman'],
+      ['name' => 'Black Canary'],
+      ['name' => 'Black Panther'],
+      ['name' => 'Captain America'],
+      ['name' => 'Catwoman'],
+      ['name' => 'Conan the Barbarian'],
+      ['name' => 'Daredevil'],
+      ['name' => 'The Defenders'],
+      ['name' => 'Doc Savage'],
+      ['name' => 'Doctor Strange'],
+      ['name' => 'Elektra'],
+      ['name' => 'Fantastic Four']
     ];
 
-    $ourHeroes = '';
-
-    foreach($heroes as $hero) {
-      $ourHeroes .= '<li>' . $hero['name'] . '</li>';
-    }
-
     return [
-      '#type' => 'markup',
-      '#markup' => '<h4>' . $this->t('These are the bast voted heroes') . '</h4>'  . '<ol>' . $ourHeroes . '</ol>'
+      '#theme' => 'hero_list',
+      '#items' => $heroes,
+      '#title' => $this->t('Our wonderful heroes list')
     ];
   }
 }
